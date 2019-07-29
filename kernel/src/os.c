88c04db42ca6f8944f8aa0b52d10be0df83063df
tracer-nju <tracer@nju.edu.cn>
2019-07-29 15:25:10 +0800

    Static hostname: ubuntu    Pretty hostname: Ubuntu          Icon name: computer-laptop            Chassis: laptop         Machine ID: d10ce222b43346f8a7c9e6b280c9cf0e            Boot ID: 1796d21a3cf74b81bc5a5b15ef72c180   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-25-generic       Architecture: x86-64  15:25:10 up 1 day, 22:48,  1 user,  load average: 1.09, 0.71, 0.61

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -43,7 +43,7 @@ static void os_init() {
   //_vme_init(pmm->alloc, pmm->free);
   dev->init();
   //printf("os_init\n");
-  //kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty1");
+  kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty1");
   //kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty2");
   //kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty3");
   //kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty4");
