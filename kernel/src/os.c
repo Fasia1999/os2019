e1bc7fe63cf3066624fba2c8e9d5246b2c39617a
tracer-nju <tracer@nju.edu.cn>
2019-07-30 19:26:10 +0800

    Static hostname: ubuntu    Pretty hostname: Ubuntu          Icon name: computer-laptop            Chassis: laptop         Machine ID: d10ce222b43346f8a7c9e6b280c9cf0e            Boot ID: 1796d21a3cf74b81bc5a5b15ef72c180   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-25-generic       Architecture: x86-64  19:26:10 up 3 days,  2:49,  1 user,  load average: 0.96, 0.71, 0.76

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -38,18 +38,17 @@ void echo_task(void *name) {
 //void enable_interrupt();
 
 static void os_init() {
-  //enable_interrupt();
   pmm->init();
   //printf("os_init_kmt1\n");
   kmt->init();
   //printf("os_init_kmt2\n");
   //_vme_init(pmm->alloc, pmm->free);
   dev->init();
-  enable_interrupt();
-  _yield();
+  //enable_interrupt();
+  //_yield();
   //printf("os_init\n");
   kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty1");
-  //_yield();
+  _yield();
   //kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty2");
   //kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty3");
   //kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty4");
