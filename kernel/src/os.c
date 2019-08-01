9bb79cc9bdb71b4e4148c5a24f93cca420176f6e
tracer-nju <tracer@nju.edu.cn>
2019-08-01 16:45:04 +0800

    Static hostname: ubuntu    Pretty hostname: Ubuntu          Icon name: computer-laptop            Chassis: laptop         Machine ID: d10ce222b43346f8a7c9e6b280c9cf0e            Boot ID: 9bccf4ade4164a0595386239025e65a7   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-25-generic       Architecture: x86-64  16:45:04 up 1 day,  4:23,  1 user,  load average: 1.08, 0.82, 0.86

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -108,6 +108,7 @@ static void os_run() {
   _intr_write(1);
   //trace_status();
   while (1) {
+    printf("os_run\n");
     //printf("hello\n");
     //printf("%d\n", _intr_read());
     //_yield();
