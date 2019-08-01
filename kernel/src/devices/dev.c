57208bc15a3e39dc248515a9a93873b5a27b7c73
tracer-nju <tracer@nju.edu.cn>
2019-08-01 16:34:46 +0800

    Static hostname: ubuntu    Pretty hostname: Ubuntu          Icon name: computer-laptop            Chassis: laptop         Machine ID: d10ce222b43346f8a7c9e6b280c9cf0e            Boot ID: 9bccf4ade4164a0595386239025e65a7   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-25-generic       Architecture: x86-64  16:34:46 up 1 day,  4:13,  1 user,  load average: 1.13, 1.10, 0.93

--- kernel/src/devices/dev.c
+++ kernel/src/devices/dev.c
@@ -53,11 +53,11 @@ static void dev_init() {
   //printf("dev_init\n");
   DEVICES(CREATE);
   DEVICES(INIT);
-  printf("dev_init_input1\n");
+  //printf("dev_init_input1\n");
   kmt->create(pmm->alloc(sizeof(task_t)), "input-task", input_task, NULL);
   //_yield();
   //printf("dev_init_input2\n");
-  printf("dev_init_tty1\n");
+  //printf("dev_init_tty1\n");
   kmt->create(pmm->alloc(sizeof(task_t)), "tty-task", tty_task, NULL);
   //_yield();
   //printf("dev_init_tty2\n");
