7e1d9d291bbf224053020af5367ba3cdab7d8a57
tracer-nju <tracer@nju.edu.cn>
2019-08-02 11:28:58 +0800

    Static hostname: ubuntu    Pretty hostname: Ubuntu          Icon name: computer-laptop            Chassis: laptop         Machine ID: d10ce222b43346f8a7c9e6b280c9cf0e            Boot ID: ea90b258dbec44cdaedac4c7cef0242d   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-25-generic       Architecture: x86-64  11:28:58 up  1:35,  1 user,  load average: 0.90, 0.60, 0.47

--- kernel/src/devices/input/input.c
+++ kernel/src/devices/input/input.c
@@ -83,7 +83,7 @@ static _Context *input_notify(_Event ev, _Context *context) {
   //printf("input input input\n");
   //printf("%d\n", sem_kbdirq.count);
   //assert(0);
-  if(sem_kbdirq->count < 0)
+  if(sem_kbdirq.count < 0)
     kmt->sem_signal(&sem_kbdirq);
   return NULL;
 }
