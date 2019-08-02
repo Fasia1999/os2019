041648b4f0f0661caf8c77a191cbd706fe1fd3fe
tracer-nju <tracer@nju.edu.cn>
2019-08-02 10:53:29 +0800

    Static hostname: ubuntu    Pretty hostname: Ubuntu          Icon name: computer-laptop            Chassis: laptop         Machine ID: d10ce222b43346f8a7c9e6b280c9cf0e            Boot ID: ea90b258dbec44cdaedac4c7cef0242d   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-25-generic       Architecture: x86-64  10:53:29 up 59 min,  1 user,  load average: 0.66, 0.48, 0.49

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -111,12 +111,12 @@ static _Context *kmt_context_switch(_Event ev, _Context* context){
             }
             else
             {
-                printf("*task name*: %s, state: %d\n", tasks[index].task->name, tasks[index].task->state);
+                //printf("*task name*: %s, state: %d\n", tasks[index].task->name, tasks[index].task->state);
             }
         }
         else
         {
-            printf("*task name*: %s\n", tasks[index].task->name);
+            //printf("*task name*: %s\n", tasks[index].task->name);
         }
         kmt_spin_unlock(&entry_lock[index]);
     }
