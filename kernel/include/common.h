7e6e706e278da399e7748c2b208bd5fbb4217a56
tracer-nju <tracer@nju.edu.cn>
2019-08-05 10:16:54 +0800

    Static hostname: ubuntu    Pretty hostname: Ubuntu          Icon name: computer-laptop            Chassis: laptop         Machine ID: d10ce222b43346f8a7c9e6b280c9cf0e            Boot ID: ea90b258dbec44cdaedac4c7cef0242d   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-25-generic       Architecture: x86-64  10:16:55 up 3 days, 23 min,  1 user,  load average: 1.27, 1.00, 0.89

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -11,7 +11,13 @@
 #include<am.h>
 
 
-
+inline task_t* get_cur(void){
+    int cpu_id=_cpu();
+extern task_entry tasks[MAX_TASK];
+extern int current_id[MAX_CPU];
+    task_t* ret=tasks[current_id[cpu_id]].task;
+    return ret;
+}
 
 
 #endif
